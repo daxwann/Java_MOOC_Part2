9e7773974f4a3d5ecabd2f025a5dd61b6789d6f6
Dax Wann <daxwann@gmail.com>
2020-02-02 20:05:25 -0800

    complete week10 ex28

--- week10-week10_28.Groups/src/movable/Group.java
+++ week10-week10_28.Groups/src/movable/Group.java
@@ -1,4 +1,33 @@
 package movable;
 
-public class Group {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Group implements Movable {
+    private List<Movable> group;
+
+    public Group(){
+        this.group = new ArrayList<Movable>();
+    }
+
+    public void addToGroup(Movable movable){
+        this.group.add(movable);
+    }
+
+    public void move(int dx, int dy){
+        for (Movable item : group) {
+            item.move(dx, dy);
+        }
+    }
+
+    @Override
+    public String toString(){
+        String positions = "";
+
+        for (Movable item : group) {
+            positions += item + "\n";
+        }
+
+        return positions;
+    }
 }
