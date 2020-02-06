6d337cc659b55cd975f4482fec64c269f876d9ef
Dax Wann <daxwann@gmail.com>
2020-02-05 23:33:43 -0800

    add ContainerHistory

---
+++ week10-week10_30.Container/src/containers/ContainerHistory.java
@@ -0,0 +1,61 @@
+package containers;
+
+import java.util.ArrayList;
+import java.lang.Math;
+import java.util.Collections;
+
+public class ContainerHistory {
+    private ArrayList<Double>history;
+
+    public ContainerHistory() {
+        this.history = new ArrayList<Double>();
+    }
+
+    public void add(double situation) {
+        this.history.add(situation);
+    }
+
+    public void reset() {
+        this.history.clear();
+    }
+
+    public double maxValue() {
+        return Collections.max(this.history);
+    }
+
+    public double minValue() {
+        return Collections.min(this.history);
+    }
+
+    public double average() {
+        double sum = 0;
+
+        if (this.history.size() == 0) {
+            return 0;
+        }
+
+        for (int i = 0; i < this.history.size(); i++) {
+            sum += this.history.get(i);
+        }
+
+        return sum / this.history.size();
+    }
+
+    public double greatestFluctuation() {
+        double greatest = 0.0;
+        for (int i = 0; i < this.history.size(); i++) {
+            if (i + 1 < this.history.size()) {
+                double fluctuation = Math.abs(this.history.get(i + 1) - this.history.get(i));
+                if (fluctuation > greatest) {
+                    greatest = fluctuation;
+                }
+            }
+        }
+        return greatest;
+    }
+
+    @Override
+    public String toString() {
+        return this.history.toString();
+    }
+}
